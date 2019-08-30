11d163e6aad2baf7010168aa8811ac2d1995544b
jdeepee <joshuadparkin@gmail.com>
2019-08-30 21:24:56 +0200

    Refactored string in macros so that they can be used as string literals in format!

--- src/conductor_strings.rs
+++ src/conductor_strings.rs
@@ -1,24 +1,50 @@
-pub static GENERAL_CONDUCTOR_DATA: &str = "
-persistence_dir = \"/holochain/persistence\"
-#signing_service_uri = \"http://localhost:8888\"
-
-[[dnas]]
-file = \"/holochain/dnas/Junto/junto-rust/dist/junto-rust.dna.json\"
-id = \"junto-app\"
-hash = \"Qmdu6RjFXeYfAL8MzpGG9RGUPReo36FLeNW4bZFpVkcY2N\"
-
-#[[dnas]]
-#file = \"/holochain/dnas/DeepKey/dist/DeepKey.dna.json\"
-#id = \"deepkey\"
-#hash = \"QmdEqRWmJ7MGfxQVKJcqdzghQ19ynK7CanUeTQFMoFeiPo\"
+#[macro_export]
+macro_rules! GENERAL_CONDUCTOR_DATA { () => { 
+r#"persistence_dir = "{}/persistence"
+#signing_service_uri = "http://localhost:8888"
 
 [network]
-type=\"n3h\"
-n3h_persistence_path = \"/holochain/n3h\"
+type="n3h"
+n3h_persistence_path = "{}/n3h"
 n3h_log_level = 't'
 #bootstrap_nodes = []
-#n3h_mode = \"REAL\"
-#Agent for hosting applications";
+#n3h_mode = "REAL"
+#Agent for hosting applications"#
+}; }
+
+#[macro_export]
+macro_rules! DNA_STRING { () => { 
+r#"[[dnas]]
+file = "{}"
+id = "{}"
+hash = "{}""#
+}; }
+
+#[macro_export]
+macro_rules! AGENT_STRING { () => { 
+r#"[[agents]]
+id = "{}"
+name = "{}"
+keystore_file = "{}"
+public_address = "{}""# 
+}; }
+
+#[macro_export]
+macro_rules! INSTANCE_STRING { () => {  
+r#"[[instances]]
+agent = "{}"
+dna = "{}"
+id = "{}-{}"
+[instances.storage]
+path = "/holochain/{}/storage/{}"
+type = "file""# 
+}; }
+
+#[macro_export]
+macro_rules! INTERFACE_STRING { () => { 
+"\t[[interfaces.instances]]
+\tid = \"{}-{}\""
+}; }
 
 pub static INTERFACE_GENERAL: &str = "
 [[interfaces]]
